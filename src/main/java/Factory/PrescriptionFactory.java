9466f0c0dcb7d446954f358fb1c8280338acb651
Zaeem182 <zaeempetersen@gmail.com>
2022-04-07 17:38:35 +0200

    added Factory class, factory test and helper class for CustomerFactory and PrescriptionFactory

--- src/main/java/Factory/PrescriptionFactory.java
+++ src/main/java/Factory/PrescriptionFactory.java
@@ -1,10 +1,15 @@
 package Factory;
 
 import Entity.Prescription;
+import Util.ZaeemHelper;
 
 public class PrescriptionFactory {
 
     public static Prescription createPrescription(int prescID, int customerID, String prescType, int prescDoses){
+
+        if (ZaeemHelper.isNull(prescType)){
+           return null;
+        }
         return new Prescription.Builder().setPrescID(prescID)
                 .setCustomerID(customerID)
                 .setPrescType(prescType)

08ee8661196589b0d011f5aaec4854b33d76b735
SiphiweChauque <219084777@mycput.ac.za>
2022-04-07 04:12:28 +0200

    the entity comment for Inventory and the JobTitle Updated

---
+++ src/main/java/Factory/JobTitleFactory.java
@@ -0,0 +1,18 @@
+package Factory;
+
+import Entity.JobTitle;
+import Entity.JobTitle;
+import Util.SiphosethuHelper;
+
+public class JobTitleFactory {
+//    public static JobTitleFactory createAppointment(int employeeID, int DoctorID, int secretaryID) {
+//
+//
+//        int employeeID = SiphosethuHelper.employeeID();
+//        JobTitle jobTitle = new JobTitle().Builder().setSecretaryID(employeeID)
+//                .setDoctor(employeeID)
+//                .setSecretaryID(employeeID)
+//                .build();
+//        return JobTitle;
+//    }
+}

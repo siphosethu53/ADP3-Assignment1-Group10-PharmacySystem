6c545e822bd457d1694dd373bbad48a468ea55e8
Zaeem182 <zaeempetersen@gmail.com>
2022-08-07 15:39:56 +0200

    added .yaml and main method

---
+++ src/main/java/PharmacySystemApplication.java
@@ -0,0 +1,8 @@
+import org.springframework.boot.SpringApplication;
+
+public class PharmacySystemApplication {
+
+    public static void main (String[] args){
+        SpringApplication.run(PharmacySystemApplication.class,args);
+    }
+}

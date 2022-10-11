8b7937fce22974ac4050f3eb7ee1933ee0f5cb6b
Thabiso Matsaba <220296006@mycput.ac.za>
2022-10-11 13:17:43 +0200

    Added NameFactory and CustomerFactory classes

---
+++ src/main/java/za/ac/cput/factory/CustomerFactory.java
@@ -0,0 +1,24 @@
+package factory;
+import domain.Customer;
+import domain.Name;
+import org.hibernate.validator.internal.util.StringHelper;
+
+import java.util.UUID;
+/*CustomerFactory.java
+ * Factory for the Customer
+ * Thabiso Matsaba 220296006
+ * 10 August 202222
+ */
+public class CustomerFactory {
+
+    public static Customer build(String customerId, Name name){
+
+        if(StringHelper.isNullOrEmptyString(customerId))
+            throw new IllegalArgumentException();
+
+        if(name == null)
+            throw new IllegalArgumentException();
+
+        return new  Customer.Builder().customerId(customerId).name(name).build();
+    }
+}

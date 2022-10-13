f089c086314c5827351070e349b9e730930794fe
Thabiso Matsaba <220296006@mycput.ac.za>
2022-10-13 11:33:02 +0200

    Added CustomerService and CistomerServiceImpl

---
+++ src/main/java/za/ac/cput/service/CustomerService.java
@@ -0,0 +1,16 @@
+package za.ac.cput.service;
+import za.ac.cput.domain.Customer;
+import java.util.List;
+import java.util.Optional;
+
+/* ICustomerService.java
+ * @Author: Thabiso Matsaba (220296006)
+ * Date: 11 August 2022
+ */
+public interface CustomerService extends IService<Customer, String> {
+
+    List<Customer> findAll();
+
+    void deleteById(String id);
+
+}

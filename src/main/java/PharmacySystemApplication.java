425e5a0d6d818d1ab04bde83326f52a28cf02055
ilyaasdavids <80684345+ilyaasdavids@users.noreply.github.com>
2022-10-14 12:52:09 +0200

    Update PharmacySystemApplication.java

--- src/main/java/PharmacySystemApplication.java
+++ src/main/java/PharmacySystemApplication.java
@@ -1,7 +1,5 @@
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class PharmacySystemApplication {
 
     public static void main (String[] args){
